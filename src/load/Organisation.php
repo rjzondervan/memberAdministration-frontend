941a69b71778b5af9e01263d77525fa762876d06
Barry <barrybrands02@hotmail.com>
2019-11-07 10:50:28 +0100

    Organisation class added

---
+++ src/load/Organisation.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace load;
+
+use DateTime;
+
+class Organisation
+{
+    private $id;
+    private $name;
+    private $organisationNumber;
+    private $goal;
+
+    private function __construct(string $id)
+    {
+        $url = "http://localhost:8080/organisations/$id";
+        $json = http_get($url);
+        $organisationRaw = json_decode($json);
+    }
+
+    public function setAll(string $id,
+                           string $name,
+                           int $organisationNumber,
+                           string $goal,
+                           Address $location,
+                           Member $members
+
+    )
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+}
